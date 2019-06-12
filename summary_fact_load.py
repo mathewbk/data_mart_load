845b796ba6435514d8abda662d7a1f983467dff9
mathewbk <bmathew@email.com>
2019-06-12 21:46:25 +0800

    summary fact table load

--- summary_fact_load.py
+++ summary_fact_load.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ## ETL Job that loads Summary Fact Table
+
+# COMMAND ----------
+
 from pyspark.sql.functions import regexp_replace
 from pyspark.sql.functions import unix_timestamp
 from pyspark.sql.functions import col
@@ -19,6 +24,6 @@ parsed_fields.registerTempTable("summary_fact_temp")
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC drop table if exists bmathew.summary_fact;
-# MAGIC create table bmathew.summary_fact using delta as (select country, browser, count(1) as page_views from summary_fact_temp group by 1,2
+# MAGIC drop table if exists bmathew.summary_fact_load;
+# MAGIC create table bmathew.summary_fact_load using delta as (select country, browser, count(1) as page_views from summary_fact_temp group by 1,2
 # MAGIC );
